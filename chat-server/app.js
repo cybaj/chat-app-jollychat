75e66d47c6090ce2168882341b4f390d5e169b20
cybaj <cybaj@naver.com>
2019-05-22 23:48:26 +0900

    [env setting completed] [axis nlp 서버 명사추출 api]

--- chat-server/app.js
+++ chat-server/app.js
@@ -3,6 +3,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server,{
   pingTimeout: 1000,
 });
+const axios = require('axios')
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,6 +31,20 @@ io.on('connection', function(socket){
 
     // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
     socket.broadcast.emit('chat', msg);
+
+    // NLP 서버에 보낸다
+    axios.post('http://localhost:5001/api/noun_extract/123', {
+      'sent': data.msg,
+      'speaker': data.name
+    })
+    .then((res) => {
+      console.log('statusCode: ${res.statusCode}')
+      console.log(res)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
   });
   
   socket.on('disconnect', function() {
