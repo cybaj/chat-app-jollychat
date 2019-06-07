9e06f025ccbed4611b8571a557c0d66da6e3a031
cybaj <cybaj@naver.com>
2019-06-07 17:07:52 +0900

    fixed

--- chat-front/src/plugins/socketPlugin.js
+++ chat-front/src/plugins/socketPlugin.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 
-const socket = io('http://192.168.43.177:3000');
+// const socket = io('http://192.168.43.177:3000');
+const socket = io('http://localhost:3000');
 
 
 const SocketPlugin = {
